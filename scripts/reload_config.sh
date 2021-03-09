e7b54c6c8e721a43d5cce737fe08fd9f119e6306
Shawn McNaughton <shawngmc@gmail.com>
2021-03-09 06:36:17 +0000

    Fixed reload script for caddy2

--- scripts/reload_config.sh
+++ scripts/reload_config.sh
@@ -3,8 +3,6 @@
 export CADDY_CONTAINER=$(docker ps -a -q --filter="name=caddy")
 export CADDY_PID=$(docker exec -it $CADDY_CONTAINER pgrep caddy)
 CADDY_PID=${CADDY_PID//[$'\t\r\n ']}
-export KILL_COMMAND="kill -SIGUSR1 $CADDY_PID"
 echo "Reloading config in $CADDY_CONTAINER at PID $CADDY_PID..."
-echo "Using command: $KILL_COMMAND"
-docker exec -it $CADDY_CONTAINER $KILL_COMMAND
+docker exec -it $CADDY_CONTAINER caddy reload --config /etc/caddy/Caddyfile
 echo "Reload signal complete..."
